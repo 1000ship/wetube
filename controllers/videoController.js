605fd7adec5dc3cf3fac5192de2f2d0b7dbf98ce
Seong-hyeok Cheon <cjstjdgur123@naver.com>
2019-07-31 15:38:39 +0900

    2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,30 @@
-export const home = (req, res) => res.render("home")
-export const search = (req, res) => res.send("Search")
-export const videos = (req, res) => res.send("Videos")
-export const upload = (req, res) => res.send("Upload")
-export const videoDetail = (req, res) => res.send("VideoDetail")
-export const editVideo = (req, res) => res.send("EditVideo")
-export const deleteVideo = (req, res) => res.send("DeleteVideo")
+export const home = (req, res) => res.render("home", {pageTitle: "Home", potato: 12345})
+
+export const search = (req, res) => {
+    const {
+        query: { term: searchingBy }
+    } = req;
+    res.render("search",
+        {pageTitle: "Search", searchingBy })
+    
+}
+
+export const videos = (req, res) => {
+    res.render("Videos", {pageTitle: "Video"})
+}
+
+export const upload = (req, res) => {
+    res.render("Upload", {pageTitle: "Upload"})
+}
+
+export const videoDetail = (req, res) => {
+    res.render("VideoDetail", {pageTitle: "Video Detail"})
+}
+
+export const editVideo = (req, res) => {
+    res.render("EditVideo", {pageTitle: "Edit Video"})
+}
+
+export const deleteVideo = (req, res) => {
+    res.render("DeleteVideo", {pageTitle: "Delete Video"})
+}

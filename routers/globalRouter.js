c7ddec3963e204c2f2eca0f2cbea44aaf1c21651
1000ship <cjstjdgur1234@gmail.com>
2020-01-07 02:07:22 +0900

    6.2 Local Authentication with Passport

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -6,10 +6,13 @@ import { logout, postJoin, getJoin, postLogin, getLogin } from '../controllers/u
 const globalRouter = express.Router();
 globalRouter.get(routes.home, home)
 globalRouter.get(routes.search, search)
+
 globalRouter.get(routes.join, getJoin)
 globalRouter.post(routes.join, postJoin)
+
 globalRouter.get(routes.login, getLogin)
 globalRouter.post(routes.login, postLogin)
+
 globalRouter.get(routes.logout, logout)
 
 export default globalRouter

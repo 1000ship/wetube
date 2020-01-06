4fba4be633930da06a78bda6c4332e7fd5135b84
1000ship <cjstjdgur1234@gmail.com>
2020-01-06 23:55:25 +0900

    3.12 Searching Videos

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import routes from "../routes";
-import { home, search } from "../controllers/videoController"
-import { logout, postJoin, getJoin, postLogin, getLogin } from "../controllers/userController"
+import express from 'express';
+import routes from '../routes';
+import { home, search } from '../controllers/videoController';
+import { logout, postJoin, getJoin, postLogin, getLogin } from '../controllers/userController';
 
 const globalRouter = express.Router();
 globalRouter.get(routes.home, home)
